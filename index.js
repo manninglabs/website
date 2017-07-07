18a3fcbaaffae4e45def1c334ede2bd3800e8e99
Лаптев Артем <matvejlaptev@MacBook-Pro-matvej.local>
2017-07-07 16:47:00 +0300

    Draft version of the site

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-$('.sl').slick({
+$('.slider-for-projects').slick({
     autoplay: true,
     autoplaySpeed: 5000,
     dots: true,
     fade: true
+});
+
+$('.slider-for-employee').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true
 });
