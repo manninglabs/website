4cb45006dc0be1a3a627010f22ddf111b6333752
Лаптев Артем <matvejlaptev@MacBook-Pro-matvej.local>
2017-07-10 10:14:32 +0300

    Created accordeon

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-$('.slider-for-projects').slick({
+//sliders
+
+$('.slider-for-projects').slick({ 
     autoplay: true,
     autoplaySpeed: 5000,
     dots: true,
@@ -9,4 +11,12 @@ $('.slider-for-employee').slick({
     autoplay: true,
     autoplaySpeed: 5000,
     dots: true
+});
+
+//accordeon
+$(document).ready(function(){
+    $(".accordeon p").hide().prev().click(function() {
+        $(".accordeon p").slideUp();
+        $(this).next().not(":visible").slideDown();
+    });
 });
